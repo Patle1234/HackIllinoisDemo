ed5915ef99f847ba5bd29e821f442361c7b60cd2
Dev Patel <63603475+Patle1234@users.noreply.github.com>
2023-07-03 11:30:02 -0500

    Initial Commit

---
+++ HackIllinoisSystemsDemo/HackIllinoisSystemsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackIllinoisSystemsDemoApp.swift
+//  HackIllinoisSystemsDemo
+//
+//  Created by Dev Patel on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct HackIllinoisSystemsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
